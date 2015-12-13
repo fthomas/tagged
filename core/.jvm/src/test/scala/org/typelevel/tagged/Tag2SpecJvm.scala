a4004f2ad0634d47b3bdfb61bb36d7f8e003a431
Frank S. Thomas <frank@timepit.eu>
2015-12-13 14:19:44 +0100

    Check javap output for tag2 and tag3

--- core/.jvm/src/test/scala/org/typelevel/tagged/Tag2SpecJvm.scala
+++ core/.jvm/src/test/scala/org/typelevel/tagged/Tag2SpecJvm.scala
@@ -3,49 +3,49 @@ package org.typelevel.tagged
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
 import org.typelevel.tagged.TestUtils._
-import org.typelevel.tagged.tag1._
+import org.typelevel.tagged.tag2._
 
-class Tag1DisassembledAnyVal {
+class Tag2DisassembledAnyVal {
   val base: Int = 1
   val tagged: Int @@ SomeTag = tag(1)
   val untagged: Int = untag(tagged)
   val arrayWithTagged: Array[Int @@ SomeTag] = Array(tag(1), tag(2))
 }
 
-class Tag1DisassembledAnyRef {
+class Tag2DisassembledAnyRef {
   val base: String = "1"
   val tagged: String @@ SomeTag = tag("1")
   val untagged: String = untag(tagged)
   val arrayWithTagged: Array[String @@ SomeTag] = Array(tag("1"), tag("2"))
 }
 
-object Tag1SpecJvm extends Properties("tag1") {
+object Tag2SpecJvm extends Properties("tag2") {
 
   property("javap.AnyVal") = secure {
-    val actual = javapOutput(new Tag1DisassembledAnyVal)
+    val actual = javapOutput(new Tag2DisassembledAnyVal)
     val expected =
-      """Compiled from "Tag1SpecJvm.scala"
-        |public class org.typelevel.tagged.Tag1DisassembledAnyVal {
+      """Compiled from "Tag2SpecJvm.scala"
+        |public class org.typelevel.tagged.Tag2DisassembledAnyVal {
         |  public int base();
         |  public java.lang.Object tagged();
         |  public int untagged();
         |  public java.lang.Object[] arrayWithTagged();
-        |  public org.typelevel.tagged.Tag1DisassembledAnyVal();
+        |  public org.typelevel.tagged.Tag2DisassembledAnyVal();
         |}
       """.stripMargin.trim
     actual ?= expected
   }
 
   property("javap.AnyRef") = secure {
-    val actual = javapOutput(new Tag1DisassembledAnyRef)
+    val actual = javapOutput(new Tag2DisassembledAnyRef)
     val expected =
-      """Compiled from "Tag1SpecJvm.scala"
-        |public class org.typelevel.tagged.Tag1DisassembledAnyRef {
+      """Compiled from "Tag2SpecJvm.scala"
+        |public class org.typelevel.tagged.Tag2DisassembledAnyRef {
         |  public java.lang.String base();
-        |  public java.lang.String tagged();
+        |  public java.lang.Object tagged();
         |  public java.lang.String untagged();
-        |  public java.lang.String[] arrayWithTagged();
-        |  public org.typelevel.tagged.Tag1DisassembledAnyRef();
+        |  public java.lang.Object[] arrayWithTagged();
+        |  public org.typelevel.tagged.Tag2DisassembledAnyRef();
         |}
       """.stripMargin.trim
     actual ?= expected
--- core/.jvm/src/test/scala/org/typelevel/tagged/Tag3SpecJvm.scala
+++ core/.jvm/src/test/scala/org/typelevel/tagged/Tag3SpecJvm.scala
@@ -3,49 +3,49 @@ package org.typelevel.tagged
 import org.scalacheck.Prop._
 import org.scalacheck.Properties
 import org.typelevel.tagged.TestUtils._
-import org.typelevel.tagged.tag1._
+import org.typelevel.tagged.tag3._
 
-class Tag1DisassembledAnyVal {
+class Tag3DisassembledAnyVal {
   val base: Int = 1
   val tagged: Int @@ SomeTag = tag(1)
   val untagged: Int = untag(tagged)
   val arrayWithTagged: Array[Int @@ SomeTag] = Array(tag(1), tag(2))
 }
 
-class Tag1DisassembledAnyRef {
+class Tag3DisassembledAnyRef {
   val base: String = "1"
   val tagged: String @@ SomeTag = tag("1")
   val untagged: String = untag(tagged)
   val arrayWithTagged: Array[String @@ SomeTag] = Array(tag("1"), tag("2"))
 }
 
-object Tag1SpecJvm extends Properties("tag1") {
+object Tag3SpecJvm extends Properties("tag3") {
 
   property("javap.AnyVal") = secure {
-    val actual = javapOutput(new Tag1DisassembledAnyVal)
+    val actual = javapOutput(new Tag3DisassembledAnyVal)
     val expected =
-      """Compiled from "Tag1SpecJvm.scala"
-        |public class org.typelevel.tagged.Tag1DisassembledAnyVal {
+      """Compiled from "Tag3SpecJvm.scala"
+        |public class org.typelevel.tagged.Tag3DisassembledAnyVal {
         |  public int base();
-        |  public java.lang.Object tagged();
+        |  public int tagged();
         |  public int untagged();
-        |  public java.lang.Object[] arrayWithTagged();
-        |  public org.typelevel.tagged.Tag1DisassembledAnyVal();
+        |  public int[] arrayWithTagged();
+        |  public org.typelevel.tagged.Tag3DisassembledAnyVal();
         |}
       """.stripMargin.trim
     actual ?= expected
   }
 
   property("javap.AnyRef") = secure {
-    val actual = javapOutput(new Tag1DisassembledAnyRef)
+    val actual = javapOutput(new Tag3DisassembledAnyRef)
     val expected =
-      """Compiled from "Tag1SpecJvm.scala"
-        |public class org.typelevel.tagged.Tag1DisassembledAnyRef {
+      """Compiled from "Tag3SpecJvm.scala"
+        |public class org.typelevel.tagged.Tag3DisassembledAnyRef {
         |  public java.lang.String base();
         |  public java.lang.String tagged();
         |  public java.lang.String untagged();
         |  public java.lang.String[] arrayWithTagged();
-        |  public org.typelevel.tagged.Tag1DisassembledAnyRef();
+        |  public org.typelevel.tagged.Tag3DisassembledAnyRef();
         |}
       """.stripMargin.trim
     actual ?= expected
